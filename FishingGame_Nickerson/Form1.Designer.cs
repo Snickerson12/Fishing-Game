3ac0d86db852e20e77f848b7e72c93b81437e837
Snickerson12 <laura.nickerson@gmail.com>
2020-02-10 17:35:50 -0600

    C# fishing game from programming for game design

---
+++ FishingGame_Nickerson/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace FishingGame_Nickerson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Fish7 = new System.Windows.Forms.PictureBox();
+            this.Fish2 = new System.Windows.Forms.PictureBox();
+            this.Fish9 = new System.Windows.Forms.PictureBox();
+            this.Fish3 = new System.Windows.Forms.PictureBox();
+            this.Fish4 = new System.Windows.Forms.PictureBox();
+            this.Fish10 = new System.Windows.Forms.PictureBox();
+            this.Fish6 = new System.Windows.Forms.PictureBox();
+            this.Fish8 = new System.Windows.Forms.PictureBox();
+            this.Fish5 = new System.Windows.Forms.PictureBox();
+            this.Fish1 = new System.Windows.Forms.PictureBox();
+            this.Boat = new System.Windows.Forms.PictureBox();
+            this.MoveCount = new System.Windows.Forms.Label();
+            this.FishCount = new System.Windows.Forms.Label();
+            this.MoveCountLabel = new System.Windows.Forms.Label();
+            this.FishCountLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Boat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel1.Controls.Add(this.Fish7);
+            this.panel1.Controls.Add(this.Fish2);
+            this.panel1.Controls.Add(this.Fish9);
+            this.panel1.Controls.Add(this.Fish3);
+            this.panel1.Controls.Add(this.Fish4);
+            this.panel1.Controls.Add(this.Fish10);
+            this.panel1.Controls.Add(this.Fish6);
+            this.panel1.Controls.Add(this.Fish8);
+            this.panel1.Controls.Add(this.Fish5);
+            this.panel1.Controls.Add(this.Fish1);
+            this.panel1.Controls.Add(this.Boat);
+            this.panel1.Location = new System.Drawing.Point(33, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // Fish7
+            // 
+            this.Fish7.BackColor = System.Drawing.Color.DarkOrchid;
+            this.Fish7.Location = new System.Drawing.Point(476, 102);
+            this.Fish7.Name = "Fish7";
+            this.Fish7.Size = new System.Drawing.Size(22, 18);
+            this.Fish7.TabIndex = 10;
+            this.Fish7.TabStop = false;
+            // 
+            // Fish2
+            // 
+            this.Fish2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.Fish2.Location = new System.Drawing.Point(360, 580);
+            this.Fish2.Name = "Fish2";
+            this.Fish2.Size = new System.Drawing.Size(26, 10);
+            this.Fish2.TabIndex = 9;
+            this.Fish2.TabStop = false;
+            // 
+            // Fish9
+            // 
+            this.Fish9.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Fish9.Location = new System.Drawing.Point(42, 191);
+            this.Fish9.Name = "Fish9";
+            this.Fish9.Size = new System.Drawing.Size(23, 21);
+            this.Fish9.TabIndex = 8;
+            this.Fish9.TabStop = false;
+            // 
+            // Fish3
+            // 
+            this.Fish3.BackColor = System.Drawing.Color.Gray;
+            this.Fish3.Location = new System.Drawing.Point(186, 545);
+            this.Fish3.Name = "Fish3";
+            this.Fish3.Size = new System.Drawing.Size(23, 23);
+            this.Fish3.TabIndex = 7;
+            this.Fish3.TabStop = false;
+            // 
+            // Fish4
+            // 
+            this.Fish4.BackColor = System.Drawing.Color.Gold;
+            this.Fish4.Location = new System.Drawing.Point(420, 290);
+            this.Fish4.Name = "Fish4";
+            this.Fish4.Size = new System.Drawing.Size(19, 16);
+            this.Fish4.TabIndex = 6;
+            this.Fish4.TabStop = false;
+            // 
+            // Fish10
+            // 
+            this.Fish10.BackColor = System.Drawing.Color.Fuchsia;
+            this.Fish10.Location = new System.Drawing.Point(13, 8);
+            this.Fish10.Name = "Fish10";
+            this.Fish10.Size = new System.Drawing.Size(21, 16);
+            this.Fish10.TabIndex = 5;
+            this.Fish10.TabStop = false;
+            // 
+            // Fish6
+            // 
+            this.Fish6.BackColor = System.Drawing.Color.Firebrick;
+            this.Fish6.Location = new System.Drawing.Point(297, 185);
+            this.Fish6.Name = "Fish6";
+            this.Fish6.Size = new System.Drawing.Size(14, 15);
+            this.Fish6.TabIndex = 4;
+            this.Fish6.TabStop = false;
+            // 
+            // Fish8
+            // 
+            this.Fish8.BackColor = System.Drawing.Color.LightCyan;
+            this.Fish8.Location = new System.Drawing.Point(572, 3);
+            this.Fish8.Name = "Fish8";
+            this.Fish8.Size = new System.Drawing.Size(25, 13);
+            this.Fish8.TabIndex = 3;
+            this.Fish8.TabStop = false;
+            // 
+            // Fish5
+            // 
+            this.Fish5.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.Fish5.Location = new System.Drawing.Point(111, 355);
+            this.Fish5.Name = "Fish5";
+            this.Fish5.Size = new System.Drawing.Size(22, 20);
+            this.Fish5.TabIndex = 2;
+            this.Fish5.TabStop = false;
+            // 
+            // Fish1
+            // 
+            this.Fish1.BackColor = System.Drawing.Color.LightBlue;
+            this.Fish1.Location = new System.Drawing.Point(538, 476);
+            this.Fish1.Name = "Fish1";
+            this.Fish1.Size = new System.Drawing.Size(20, 18);
+            this.Fish1.TabIndex = 1;
+            this.Fish1.TabStop = false;
+            // 
+            // Boat
+            // 
+            this.Boat.BackColor = System.Drawing.Color.LawnGreen;
+            this.Boat.Location = new System.Drawing.Point(42, 54);
+            this.Boat.Name = "Boat";
+            this.Boat.Size = new System.Drawing.Size(46, 44);
+            this.Boat.TabIndex = 0;
+            this.Boat.TabStop = false;
+            // 
+            // MoveCount
+            // 
+            this.MoveCount.AutoSize = true;
+            this.MoveCount.Location = new System.Drawing.Point(668, 578);
+            this.MoveCount.Name = "MoveCount";
+            this.MoveCount.Size = new System.Drawing.Size(64, 13);
+            this.MoveCount.TabIndex = 1;
+            this.MoveCount.Text = "Move count";
+            // 
+            // FishCount
+            // 
+            this.FishCount.AutoSize = true;
+            this.FishCount.Location = new System.Drawing.Point(668, 629);
+            this.FishCount.Name = "FishCount";
+            this.FishCount.Size = new System.Drawing.Size(57, 13);
+            this.FishCount.TabIndex = 2;
+            this.FishCount.Text = "Fish Count";
+            // 
+            // MoveCountLabel
+            // 
+            this.MoveCountLabel.AutoSize = true;
+            this.MoveCountLabel.Location = new System.Drawing.Point(744, 578);
+            this.MoveCountLabel.Name = "MoveCountLabel";
+            this.MoveCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.MoveCountLabel.TabIndex = 3;
+            this.MoveCountLabel.Text = "0";
+            // 
+            // FishCountLabel
+            // 
+            this.FishCountLabel.AutoSize = true;
+            this.FishCountLabel.Location = new System.Drawing.Point(744, 629);
+            this.FishCountLabel.Name = "FishCountLabel";
+            this.FishCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.FishCountLabel.TabIndex = 4;
+            this.FishCountLabel.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(667, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Fishing Game";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 690);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FishCountLabel);
+            this.Controls.Add(this.MoveCountLabel);
+            this.Controls.Add(this.FishCount);
+            this.Controls.Add(this.MoveCount);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Fishing";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MoveBoat);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Fish7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fish1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Boat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Boat;
+        private System.Windows.Forms.Label MoveCount;
+        private System.Windows.Forms.Label FishCount;
+        private System.Windows.Forms.Label MoveCountLabel;
+        private System.Windows.Forms.Label FishCountLabel;
+        private System.Windows.Forms.PictureBox Fish1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Fish10;
+        private System.Windows.Forms.PictureBox Fish6;
+        private System.Windows.Forms.PictureBox Fish8;
+        private System.Windows.Forms.PictureBox Fish5;
+        private System.Windows.Forms.PictureBox Fish7;
+        private System.Windows.Forms.PictureBox Fish2;
+        private System.Windows.Forms.PictureBox Fish9;
+        private System.Windows.Forms.PictureBox Fish3;
+        private System.Windows.Forms.PictureBox Fish4;
+    }
+}
+
---
+++ FishingGame_Nickerson/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FishingGame_Nickerson
+{
+    public partial class Form1 : Form
+    {
+        private int boatMoves = 0;
+        private int fishcount = 0;
+        PictureBox[] Fish;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Fish = new PictureBox[] { Fish1, Fish2, Fish3, Fish4, Fish5, Fish6, Fish7, Fish8, Fish9, Fish10 };
+            foreach (PictureBox f in Fish) 
+            { 
+                f.Visible = true;
+            }
+            foreach (PictureBox location in Fish)
+            {
+                Random top = new Random();
+                int test = top.Next(0, 430);
+                location.Top = test;
+                Thread.Sleep(1);
+
+                int test2 = top.Next(0, 400);
+                location.Left = test2;
+                Thread.Sleep(1);
+            }
+            
+        }
+        private void MoveBoat(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    IncrementMoveCount();
+                    if(Boat.Left < 554)
+                    {
+                        Boat.Left += 10;
+                    }
+                    break;
+
+                case Keys.Left:
+                    IncrementMoveCount();
+                    if(Boat.Left > 0)
+                    {
+                        Boat.Left += -10;
+                    }
+                    break;
+
+                case Keys.Down:
+                    IncrementMoveCount();
+                    if(Boat.Top < 556)
+                    {
+                        Boat.Top += 10;
+                    } 
+                    break;
+
+                case Keys.Up:
+                    IncrementMoveCount();
+                    if(Boat.Top > 0)
+                    {
+                        Boat.Top += -10;
+                    }
+                    break;            
+            }
+ 
+            for (int i = 0; i < Fish.Length; i++)
+            {
+                if (Caughtfish(Fish[i]))
+                {
+                    IncrementFishCount();
+                    Fish[i].Visible = false;
+                }
+            }    
+        }
+
+        private void IncrementMoveCount()
+        {
+            boatMoves++;
+            MoveCountLabel.Text = boatMoves.ToString();
+        }
+
+        private void IncrementFishCount()
+        {
+            fishcount++;
+            FishCountLabel.Text = fishcount.ToString();
+            if (fishcount == 10)
+            {
+                MessageBox.Show("10 Fish found! You win!");
+            }   
+        }
+        private bool Caughtfish(PictureBox fish)
+        {
+            return (Boat.Top <= fish.Top && Boat.Bottom >= fish.Bottom && Boat.Left <= fish.Left && Boat.Right >= fish.Right && fish.Visible == true);
+        }
+    }
+}
+
